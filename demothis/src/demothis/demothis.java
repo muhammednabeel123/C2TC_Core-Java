c60ad493d11a594687bce0030c07c6d6db30bdec
Muhammed Nabeel <muhammednabeel237@gmail.com>
2022-04-07 23:16:53 +0530

    this1 commit

---
+++ demothis/src/demothis/demothis.java
@@ -0,0 +1,15 @@
+package demothis;
+
+public class demothis {
+		void m()
+				{
+			System.out.println(this);
+				}
+	public static void main(String args[]) {
+		demothis obj=new demothis();
+		obj.m();
+	}
+
+	}
+
+
